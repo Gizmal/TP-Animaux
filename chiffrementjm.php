8390a543a94d758e5f226c94b314eb1a35b2e8e0
stagiaire Dev Soissons <stagiaire_ds@sjt.idf>
2022-08-22 15:57:13 +0200

    fichier chiffrement corriger

--- chiffrementjm.php
+++ chiffrementjm.php
@@ -15,8 +15,14 @@
 
 
     $REC = $dbh->prepare("INSERT INTO users VALUES(:pseudo, :email, :password)");
-    $REC->bindParam (1, $pseudo, PDO::PARAM_STR);
-    $REC->bindParam (2, $email, PDO::PARAM_STR);
-    $REC->bindParam (3, $password, PDO::PARAM_STR);
+    // correction
+    // $REC->bindParam (1, $pseudo, PDO::PARAM_STR);
+    $REC->bindParam (:pseudo, $pseudo, PDO::PARAM_STR);
+        // correction
+    // $REC->bindParam (2, $email, PDO::PARAM_STR);
+    $REC->bindParam (:email, $email, PDO::PARAM_STR);
+        // correction
+    // $REC->bindParam (3, $password, PDO::PARAM_STR);
+    $REC->bindParam (:password, $password, PDO::PARAM_STR);
     $req->execute();
 ?>
